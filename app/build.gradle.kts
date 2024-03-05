e9efa649261a2e4bbdeb8f0aee181108046e9c59
ArturCass <artur.jalles@picpay.com>
2024-03-05 16:44:54 -0300

    message e conexão com firebase básico

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+
+    // Add the Google services Gradle plugin
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -36,6 +39,7 @@ android {
     viewBinding {
         enable = true
     }
+
 }
 
 dependencies {
@@ -44,6 +48,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-messaging:23.4.1")
+    implementation("com.google.firebase:firebase-firestore:24.10.3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -54,5 +60,9 @@ dependencies {
 
     //ImageViews arredondadas
     implementation("com.makeramen:roundedimageview:2.3.0")
+
+    //Firebase
+    implementation(platform("com.google.firebase:firebase-bom:32.7.3"))
+    implementation("com.google.firebase:firebase-analytics")
 
 }
--- app/src/main/java/com/example/chat/activities/SignInActivity.kt
+++ app/src/main/java/com/example/chat/activities/SignInActivity.kt
@@ -1,12 +1,26 @@
 package com.example.chat.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.chat.R
+import com.example.chat.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignInBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setListeners()
     }
+
+    private fun setListeners(){
+        binding.textCreateNewAccount.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
+    }
+
 }
