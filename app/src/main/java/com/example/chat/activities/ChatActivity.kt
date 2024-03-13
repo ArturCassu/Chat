80e57dd8494223b2b7da387b457f108ccbf117ef
ArturCass <artur.jalles@picpay.com>
2024-03-13 14:41:17 -0300

    layout do chat concluido

---
+++ app/src/main/java/com/example/chat/activities/ChatActivity.kt
@@ -0,0 +1,16 @@
+package com.example.chat.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.chat.R
+import com.example.chat.databinding.ActivityChatBinding
+
+class ChatActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityChatBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityChatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
