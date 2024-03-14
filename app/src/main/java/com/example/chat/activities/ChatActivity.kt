9446931f376ac155e5d84d089fbd0aa190517160
ArturCass <artur.jalles@picpay.com>
2024-03-14 17:13:24 -0300

    Mensagens em tempo real

--- app/src/main/java/com/example/chat/activities/ChatActivity.kt
+++ app/src/main/java/com/example/chat/activities/ChatActivity.kt
@@ -5,19 +5,26 @@ import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Base64
+import android.view.View
 import com.example.chat.adapters.ChatAdapter
 import com.example.chat.databinding.ActivityChatBinding
 import com.example.chat.models.ChatMessage
 import com.example.chat.models.User
 import com.example.chat.utils.Constants
 import com.example.chat.utils.PreferenceManager
+import com.google.firebase.firestore.DocumentChange
+import com.google.firebase.firestore.EventListener
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
+import java.text.SimpleDateFormat
+import java.util.Collections
 import java.util.Date
+import java.util.Locale
 
 class ChatActivity : AppCompatActivity() {
     private lateinit var binding: ActivityChatBinding
     private lateinit var receiverUser: User
-    private lateinit var chatMessages: List<ChatMessage>
+    private lateinit var chatMessages: MutableList<ChatMessage>
     private lateinit var chatAdapter: ChatAdapter
     private lateinit var preferenceManager: PreferenceManager
     private lateinit var database: FirebaseFirestore
@@ -29,7 +36,49 @@ class ChatActivity : AppCompatActivity() {
         setListners()
         loadReceiverDetails()
         init()
+        listenMessages()
 
+    }
+
+    private fun listenMessages(){
+        database.collection(Constants.KEY_COLLECTION_CHAT)
+            .whereEqualTo(Constants.KEY_SENDER_ID, preferenceManager.getString(Constants.KEY_USER_ID))
+            .whereEqualTo(Constants.KEY_RECEIVER_ID, receiverUser.id)
+            .addSnapshotListener(eventListener)
+        database.collection(Constants.KEY_COLLECTION_CHAT)
+            .whereEqualTo(Constants.KEY_SENDER_ID, receiverUser.id)
+            .whereEqualTo(Constants.KEY_RECEIVER_ID, preferenceManager.getString(Constants.KEY_USER_ID))
+            .addSnapshotListener(eventListener)
+    }
+
+    private val eventListener  = EventListener<QuerySnapshot>{ value: QuerySnapshot?, error:Throwable? ->
+        if (error != null){
+
+        }
+        if(value != null){
+            val count = chatMessages.size
+            for (document in value.documentChanges){
+                if(document.type == DocumentChange.Type.ADDED){
+                    val chatMessage = ChatMessage(
+                        document.document.getString(Constants.KEY_SENDER_ID)!!,
+                        document.document.getString(Constants.KEY_RECEIVER_ID)!!,
+                        document.document.getString(Constants.KEY_MESSAGE)!!,
+                        getReadableDateTime(document.document.getDate(Constants.KEY_TIMESTAMP)!!),
+                        document.document.getDate(Constants.KEY_TIMESTAMP)!!
+                    )
+                    chatMessages.add(chatMessage)
+                }
+            }
+            chatMessages.sortWith(Comparator { obj1:ChatMessage, obj2:ChatMessage -> obj1.dateObject.compareTo(obj2.dateObject) })
+            if (count == 0){
+                chatAdapter.notifyDataSetChanged()
+            }else{
+                chatAdapter.notifyItemRangeInserted(chatMessages.size, chatMessages.size)
+                binding.chatRecyclerView.smoothScrollToPosition(chatMessages.size - 1)
+            }
+            binding.chatRecyclerView.visibility = View.VISIBLE
+        }
+        binding.progressBar.visibility = View.GONE
     }
 
     private fun sendMessage(){
@@ -68,6 +117,10 @@ class ChatActivity : AppCompatActivity() {
     private fun setListners() = with(binding){
         imageBack.setOnClickListener{ onBackPressedDispatcher.onBackPressed() }
         layoutSend.setOnClickListener{ sendMessage() }
+    }
+
+    private fun getReadableDateTime(date: Date): String{
+        return SimpleDateFormat("MMMM dd, yyyy - hh:mm a", Locale.getDefault()).format(date)
     }
 
 }
